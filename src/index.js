0c3e5a8bfb54a82c9a3718142a34e8a9a3f24b1a
FIOdawa <159128217+FIOdawa@users.noreply.github.com>
2024-08-05 11:42:44 +0300

    Added basic JavaScript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateSong(event) {
+  event.preventDefault();
 
+  new Typewriter(".lyrics", {
+    strings: " Surprise 모두 기립 박수 (hala hala) Suicide squad 현실판에 ",
+    autoStart: true,
+    delay: 0,
+    cursor: "",
+  });
+}
+
+let songFormElement = document.querySelector(".song-generator-form");
+songFormElement.addEventListener("submit", generateSong);
